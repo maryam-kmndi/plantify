03963ae436b8a2a61913eaaa22422da2bae169df
Mary <maryamkmndi8@gmail.com>
2024-07-23 15:38:01 +0330

    Creating a layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="red">Button</Button>;
+  return (
+    <Grid templateAreas={`'header' 'main' 'footer'`} templateRows={'100px 900px 50px'}>
+      <GridItem area={"header"} bg="lightblue">
+        header
+      </GridItem>
+      <GridItem area={"main"} bg="lightgreen">
+        main
+      </GridItem>
+      <GridItem area={"footer"} bg="lightsalmon">
+        footer
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
