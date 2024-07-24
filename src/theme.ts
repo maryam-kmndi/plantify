6fba9d639bbda613bd564b419bbcac65f9f9a869
Mary <maryamkmndi8@gmail.com>
2024-07-24 21:29:03 +0330

    Implement dark mode

--- src/theme.ts
+++ src/theme.ts
@@ -1,17 +1,22 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 import "@fontsource/roboto";
 import "@fontsource/open-sans";
 
+const config: ThemeConfig = {
+  initialColorMode: "light",
+};
+
 const theme = extendTheme({
-  fonts: {
-    heading: `'roboto','open-sans'`,
-    body: `'roboto','open-sans'`,
-  },
+  config,
   colors: {
     primaryColor: "#79A141",
     secondryColor: "#FEFEFE",
     cartsColor: "#F5F3F4",
     textColor: "#464646",
+  },
+  fonts: {
+    heading: `'roboto','open-sans'`,
+    body: `'roboto','open-sans'`,
   },
 });
 
